d12f7e6a500ac5bf66e3b174d6bb0f1cedbb8870
akaz <82073147+akazdayo@users.noreply.github.com>
2023-02-19 18:32:06 +0900

    streamlitに対応しました。

--- main.py
+++ main.py
@@ -2,114 +2,159 @@ import cv2
 from PIL import Image
 import numpy as np
 import streamlit as st
+from concurrent.futures.thread import ThreadPoolExecutor
 
 
-def mosaic(src, ratio=0.1):
-    small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
-    return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+class Converter():
+    def __init__(self) -> None:
+        # self.file_name = 'data/img/dango.png'
+        # self.pic = cv2.imread(self.file_name)
+        # self.h, self.w = self.pic.shape[:2]
+        # self.pic = cv2.cvtColor(self.pic, cv2.COLOR_BGR2RGB)
+        # self.pic = self.mosaic(src=self.pic)
+        # self.RGB = [[], [], []]
+        pass
 
+    def detect_color(self, rgb):
+        # RGB値の各要素を変数に格納します。
+        r, g, b = rgb[0], rgb[1], rgb[2]
 
-file_name = 'data/img/dango.png'
-pic = cv2.imread(file_name)
-h, w = pic.shape[:2]
-pic = cv2.cvtColor(pic, cv2.COLOR_BGR2RGB)
-pic = mosaic(src=pic)
-RGB = [[], [], []]
+        # RGB値を0~1の範囲に変換します。
+        r /= 255
+        g /= 255
+        b /= 255
 
-st.title("PixelArt-Converter")
+        # RGB値の輝度（明度）を計算します。
+        y = 0.2126 * r + 0.7152 * g + 0.0722 * b
 
+        # 輝度が一定の値以下であれば黒と判定し、一定の値以上であれば白と判定します。
+        if y <= 0.1:
+            return [0, 0, 0]
+        elif y >= 0.9:
+            return [255, 255, 255]
 
-def detect_color(rgb):
-    # RGB値の各要素を変数に格納します。
-    r, g, b = rgb[0], rgb[1], rgb[2]
+        # 以下の処理は、色相（Hue）と彩度（Saturation）を計算する部分です。
+        cmax = max(r, g, b)
+        cmin = min(r, g, b)
+        delta = cmax - cmin
 
-    # RGB値を0~1の範囲に変換します。
-    r /= 255
-    g /= 255
-    b /= 255
+        h = 0
+        s = 0
+        l = (cmax + cmin) / 2
 
-    # RGB値の輝度（明度）を計算します。
-    y = 0.2126 * r + 0.7152 * g + 0.0722 * b
+        if delta != 0:
+            if cmax == r:
+                h = (g - b) / delta % 6
+            elif cmax == g:
+                h = (b - r) / delta + 2
+            elif cmax == b:
+                h = (r - g) / delta + 4
 
-    # 輝度が一定の値以下であれば黒と判定し、一定の値以上であれば白と判定します。
-    if y <= 0.1:
+            s = delta / (1 - abs(2 * l - 1))
+
+            if s < 0.1:  # gray
+                return [228, 229, 227]
+            elif h < 1:  # red
+                return [255, 127, 127]
+            elif h < 2:  # orange
+                return [255, 191, 127]
+            elif h < 3:  # yellow
+                return [255, 255, 127]
+            elif h < 4:  # green
+                return [127, 255, 127]
+            elif h < 5:  # blue
+                return [127, 127, 255]
+            elif h < 6:  # purple
+                return [191, 127, 255]
+
         return [0, 0, 0]
-    elif y >= 0.9:
-        return [255, 255, 255]
 
-    # 以下の処理は、色相（Hue）と彩度（Saturation）を計算する部分です。
-    cmax = max(r, g, b)
-    cmin = min(r, g, b)
-    delta = cmax - cmin
+    def mosaic(self, src, ratio=0.1):
+        small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+        return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
 
-    h = 0
-    s = 0
-    l = (cmax + cmin) / 2
+    def generate(self, img, count):
+        print(count)
+        color = self.detect_color(rgb=[int(img[0][count]), int(img[1][count]), int(img[2][count])])
+        img[0][count] = color[0]
+        img[1][count] = color[1]
+        img[2][count] = color[2]
+        return img
 
-    if delta != 0:
-        if cmax == r:
-            h = (g - b) / delta % 6
-        elif cmax == g:
-            h = (b - r) / delta + 2
-        elif cmax == b:
-            h = (r - g) / delta + 4
+    def store_variable(self, picture, variables, h, w):
+        for k in range(3):
+            array = picture[:, :, k]
+            for j in range(h):
+                for i in range(w):
+                    variables[k].append(str(array[j, i]))
+        return variables
 
-        s = delta / (1 - abs(2 * l - 1))
+    def image_save(self, img, h, w):
+        # RGBリストをNumPy配列に変換する
+        array = np.zeros((h, w, 3), dtype=np.uint8)
+        for k in range(3):
+            for j in range(h):
+                for i in range(w):
+                    index = j * w + i
+                    array[j, i, k] = int(img[k][index])
 
-        if s < 0.1:  # gray
-            return [228, 229, 227]
-        elif h < 1:  # red
-            return [255, 127, 127]
-        elif h < 2:  # orange
-            return [255, 191, 127]
-        elif h < 3:  # yellow
-            return [255, 255, 127]
-        elif h < 4:  # green
-            return [127, 255, 127]
-        elif h < 5:  # blue
-            return [127, 127, 255]
-        elif h < 6:  # purle
-            return [191, 127, 255]
+        # NumPy配列からPillowのImageオブジェクトを作成する
+        image = Image.fromarray(array)
 
-    return [0, 0, 0]
+        # 画像を保存する
+        image.save('./data/restored.png')
+        return array
 
 
-for k in range(3):
-    array = pic[:, :, k]
-    for j in range(h):
-        for i in range(w):
-            RGB[k].append(str(array[j, i]))
+class Web():
+    def __init__(self) -> None:
+        self.converter = Converter()
+        st.title("PixelArt-Converter")
+        # self.my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+        # アップローダー
+        self.my_upload = st.file_uploader("以下からファイルアップロード", type=['jpg', 'png'])
+        # 解像度
+        self.number = st.number_input('Insert a number', min_value=0.01, max_value=1.00)
+        # カラム設定
+        col1, self.col2 = st.columns(2)
 
-progress_text = "Operation in progress. Please wait."
-my_bar = st.progress(0)
-my_bar.text(progress_text)
-percent_complete = 0
-percent = (h*w) / 100
-for i in range(h*w):
-    color = detect_color(rgb=[int(RGB[0][i]), int(RGB[1][i]), int(RGB[2][i])])
-    RGB[0][i] = color[0]
-    RGB[1][i] = color[1]
-    RGB[2][i] = color[2]
-    percent_complete += 1
-    progress = int(percent_complete/(h*w)*100)
-    my_bar.progress(progress)
+        col1.header("Original image")
+        self.col2.header("convert image")
+
+    def progressbar(self, h, w, image):
+        i = 0
+        process = st.empty()
+        my_bar = st.progress(0)
+        my_bar.text(i)
+        percent_complete = 0
+        percent = (h*w) / 100
+        for i in range(h*w):
+            image = self.converter.generate(img=image, count=i)
+            percent_complete += 1
+            progress = int(percent_complete/(h*w)*100)
+            process.text(str(percent_complete/(h*w)*100)+"%")
+            my_bar.progress(progress)
+
+        return image
 
 
-color = detect_color(rgb=[int(RGB[0][33101]), int(RGB[1][33101]), int(RGB[2][33101])])
-print(str(RGB[0][33101])+":"+str(RGB[1][33101])+":"+str(RGB[2][33101]))
+def main():
+    web = Web()
+    converter = Converter()
+    image = [[], [], []]
+    if web.my_upload is not None:
+        img = Image.open(web.my_upload,)
+        img_array = np.array(img)
+        st.image(img_array, use_column_width=None)
+        upload = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
+        height, width = upload.shape[:2]
+        upload = converter.mosaic(src=upload, ratio=web.number)
+        image = converter.store_variable(upload, image, h=height, w=width)
+        image = web.progressbar(h=height, w=width, image=image)  # generate
+        array = converter.image_save(img=image, h=height, w=width)
+        img_bytes = cv2.imencode('.jpg', array)[1].tobytes()
+        img_array2 = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
+        web.col2.image(img_array2)
 
-print(color)
 
-# RGBリストをNumPy配列に変換する
-array = np.zeros((h, w, 3), dtype=np.uint8)
-for k in range(3):
-    for j in range(h):
-        for i in range(w):
-            index = j * w + i
-            array[j, i, k] = int(RGB[k][index])
-
-# NumPy配列からPillowのImageオブジェクトを作成する
-image = Image.fromarray(array)
-
-# 画像を保存する
-image.save('restored.png')
+main()
