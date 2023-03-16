4bbb2a90f90be52d592e17245956d99cca4c7cb2
akaz <82073147+akazdayo@users.noreply.github.com>
2023-03-16 18:19:23 +0900

    処理の高速化, カラーパレットの表示

--- main.py
+++ main.py
@@ -14,18 +14,25 @@ main
 ・Web呼び出し
 ・画像を配列に変換
 ・変換後の画像を描画
+----
+追加したいもの
+csv追加したら自動的に読み込まれる(完成)
+色を確認できるようにする(完成)
+すでに変換したものは保存して、早くする(完成)
 """
 import streamlit as st
 import numpy as np
 import cv2
 from PIL import Image
 import csv
-import time
+import os
 
 
 class Converter():
     def __init__(self) -> None:
-        color_dict = []
+        self.color_dict = {}
+        self.counter = 0
+        self.counterr = 0
 
     def read_csv(self, path):
         with open(path) as f:
@@ -33,9 +40,10 @@ class Converter():
             color = [[int(v) for v in row] for row in reader]
             return color
 
-    def color_change(self, r, g, b, option):
-        # 選択されたcsvファイルを読み込む
-        color_pallet = self.read_csv("./color/"+option+".csv")
+    def color_change(self, r, g, b, color_pallet):
+        if (r, g, b) in self.color_dict:
+            self.counter += 1
+            return self.color_dict[(r, g, b)]
         # 最も近い色を見つける
         min_distance = float('inf')
         color_name = None
@@ -44,6 +52,8 @@ class Converter():
             if distance < min_distance:
                 min_distance = distance
                 color_name = color
+        self.color_dict[(r, g, b)] = color_name
+        self.counterr += 1
         return color_name
 
     def mosaic(self, img, ratio=0.1):
@@ -62,9 +72,11 @@ class Converter():
     def convert(self, img, option):
         w, h = img.shape[:2]
         changed = img.copy()
+        # 選択されたcsvファイルを読み込む
+        color_pallet = self.read_csv("./color/"+option+".csv")
         for height in range(h):
             for width in range(w):
-                color = self.color_change(img[width][height][0], img[width][height][1], img[width][height][2], option)
+                color = self.color_change(img[width][height][0], img[width][height][1], img[width][height][2], color_pallet)
                 changed[width][height][0] = color[0]  # 赤
                 changed[width][height][1] = color[1]  # 緑
                 changed[width][height][2] = color[2]  # 青
@@ -76,6 +88,13 @@ class Web():
         self.col1, self.col2 = None, None
         self.draw_text()
 
+    def file_dir(self):
+        filedir = os.listdir("./color")
+        for i in range(len(filedir)):
+            filedir[i] = filedir[i].replace(".csv", "")
+        filedir = tuple(filedir)
+        return filedir
+
     def draw_text(self):
         st.set_page_config(
             page_title="Pixelart-Converter",
@@ -83,10 +102,12 @@ class Web():
             layout="centered",
             initial_sidebar_state="expanded",
         )
+        # file_dir = os.listdir("./color")
+        fdir = self.file_dir()
         st.title("PixelArt-Converter")
         self.upload = st.file_uploader("Upload Image", type=['jpg', 'png', 'webp'])
-        self.color = st.selectbox("Select color pallet", ('pyxel', 'pastel', 'warm', 'cold', 'rainbow', 'gold', 'pale'))
-        self.slider = st.slider('Select ratio', 0.1, 1.0, 0.3, 0.05)
+        self.color = st.selectbox("Select color pallet", fdir)
+        self.slider = st.slider('Select ratio', 0.01, 1.0, 0.3, 0.01)
 
         self.col1, self.col2 = st.columns(2)
         self.col1.header("Original img")
@@ -106,7 +127,6 @@ if __name__ == "__main__":
     web = Web()
     converter = Converter()
     default = False  # サンプル画像を一度のみ表示
-
     if web.upload != None:
         with st.spinner('Wait for it...'):
             img = web.get_image()
@@ -116,8 +136,11 @@ if __name__ == "__main__":
             cimg = converter.mosaic(cimg, web.slider)
             cimg = converter.convert(cimg, web.color)
             web.col2.image(cimg)
-        st.success('Done!')
+        st.success('Done!', icon="✅")
         st.balloons()
+        print("------")
+        print(converter.counter)
+        print(converter.counterr)
     elif default == False:
         with st.spinner('Wait for it...'):
             img = Image.open("./sample/irasutoya.png")
---
+++ pages/color_sample.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import os
+import csv
+import numpy as np
+
+
+def read_csv(path):
+    with open(path) as f:
+        reader = csv.reader(f)
+        color = [[int(v) for v in row] for row in reader]
+        return color
+
+
+def file_dir():
+    filedir = os.listdir("color")
+    for i in range(len(filedir)):
+        filedir[i] = filedir[i].replace(".csv", "")
+    filedir = tuple(filedir)
+    return filedir
+
+
+def pallet_write(col, index):
+    r = pallet[index][0]
+    g = pallet[index][1]
+    b = pallet[index][2]
+    color = np.zeros((100, 100, 3), dtype=np.uint8)
+    color[:, :] = [r, g, b]
+    col.image(color)
+
+
+fdir = file_dir()
+color = st.selectbox("Select color pallet", fdir)
+pallet = read_csv("./color/"+color+".csv")
+
+for i in range(len(pallet)//4+min(1, len(pallet) % 4)):
+    col1, col2, col3, col4 = st.columns(4)
+    if i*4+0 < len(pallet):
+        pallet_write(col1, i*4+0)
+    if i*4+1 < len(pallet):
+        pallet_write(col2, i*4+1)
+    if i*4+2 < len(pallet):
+        pallet_write(col3, i*4+2)
+    if i*4+3 < len(pallet):
+        pallet_write(col4, i*4+3)
