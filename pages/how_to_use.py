543a1ad605278f28cb983c6e601c811235329f10
akaz <82073147+akazdayo@users.noreply.github.com>
2023-03-24 12:18:18 +0900

    how_to_useを更新しました

--- pages/how_to_use.py
+++ pages/how_to_use.py
@@ -6,11 +6,53 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded",
 )
-st.title("How to Use")
-st.header("Feature")
+st.markdown("""
+# pixelart-converter
+Language : [日本語](https://github.com/akazdayo/pixelart/blob/main/README-ja.md)  
+Fascinating tool to convert images into pixel art!  
+[pixelart-converter](https://pixelart.streamlit.app)
 
-st.subheader("ratio")
-st.write("The size of the dots can be set \nThe smaller the value, the larger one dot is.")
+# Basic functions
+## colorpallet
+This site converts colors.  
+Select the color palette to use when converting colors.  
+Pyxel is the color used in the library called [Pyxel](https://github.com/kitao/pyxel).  
 
-st.subheader("color pallet")
-st.write("You can specify the color palette provided.")
+## ratio
+This is a slider that can be adjusted in increments of 0.01. The lower the number, the larger the dot.
+
+## Custom Pallet
+You can create your own ColorPallet.  
+Enter the colors you want to add to the palette in the table using color codes.  
+The colors entered in the table will be displayed on the right side.  
+It is easier to select a color from the color picker above the table, copy the color code, and enter it.  
+Color picker is not supported.
+
+## Tweet
+A button to tweet to Twitter.  
+It does not support attaching images.  
+When attaching an image, please copy the image or download the image and attach it.  
+If you do not turn off the tracker blocker, it may not be displayed.  
+
+
+# Experimental Features
+This is not an official feature yet, so there may be bugs or errors.  
+## Anime Filter
+Add edges.  
+## No Color Convert
+Disables the color palette.  
+## decrease Color
+Decrease color.  
+Basically used with ``No Color Convert``.
+
+## threhsold
+Value of AnimeFilter (edge processing).  
+The smaller the value, the more edges are processed.  
+### threhsold 1
+Specifies the amount of edges.
+### threhsold 2
+Specifies the length of edges.
+
+# Color Sample
+Displays the colors in the default color palette  
+""")
