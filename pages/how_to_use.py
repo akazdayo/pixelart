8d624982ee11c7499cab179f87d03046bd65c071
akaz <82073147+akazdayo@users.noreply.github.com>
2023-03-26 21:25:57 +0900

    how to useの更新

--- pages/how_to_use.py
+++ pages/how_to_use.py
@@ -17,9 +17,11 @@ Fascinating tool to convert images into pixel art!
 This site converts colors.  
 Select the color palette to use when converting colors.  
 Pyxel is the color used in the library called [Pyxel](https://github.com/kitao/pyxel).  
+![Color pallet](./image/pallet.png)
 
 ## ratio
 This is a slider that can be adjusted in increments of 0.01. The lower the number, the larger the dot.
+![Select ratio](./image/ratio.png)
 
 ## Custom Pallet
 You can create your own ColorPallet.  
@@ -27,6 +29,7 @@ Enter the colors you want to add to the palette in the table using color codes.
 The colors entered in the table will be displayed on the right side.  
 It is easier to select a color from the color picker above the table, copy the color code, and enter it.  
 Color picker is not supported.
+![Custom pallet](./image/custom.png)
 
 ## Tweet
 A button to tweet to Twitter.  
@@ -34,16 +37,21 @@ It does not support attaching images.
 When attaching an image, please copy the image or download the image and attach it.  
 If you do not turn off the tracker blocker, it may not be displayed.  
 
-
-# Experimental Features
-This is not an official feature yet, so there may be bugs or errors.  
+# More Options
 ## Anime Filter
 Add edges.  
+![animefilter_on](./image/anime.png)
+![animefilter_off](./image/anime2.jpg)
+
 ## No Color Convert
 Disables the color palette.  
+![no_convert](./image/no_convert.jpg)
+
+
 ## decrease Color
 Decrease color.  
 Basically used with ``No Color Convert``.
+![decrease_color](./image/decrease.jpg)
 
 ## threhsold
 Value of AnimeFilter (edge processing).  
@@ -53,6 +61,12 @@ Specifies the amount of edges.
 ### threhsold 2
 Specifies the length of edges.
 
+# Experimental Features
+This is not an official feature yet, so there may be bugs or errors.  
+## Pixel Edge
+Generate edges with dots.
+
 # Color Sample
 Displays the colors in the default color palette  
+![color_sample](./image/sample.png)
 """)
